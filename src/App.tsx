96b392bd4e438f5738085dc9b1554219c04df66b
Anastasia <nastia.shrai@gmail.com>
2023-06-29 14:58:59 +0300

    feat: add CarsTable to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import CarsTable from './components/CarsTable/CarsTable';
+import { fetchCars } from './api/api';
+import { CarData } from './utils/types';
 
 function App() {
+  const [cars, setCars] = useState<CarData[]>([]);
+
+  const getCarsList = async () => {
+    const carsList = await fetchCars();
+    setCars(carsList);
+  };
+
+  useEffect(() => {
+    getCarsList();
+  }, []);
+
   return (
     <>
-      <div>
-        <h1>Cars</h1>
-      </div>
+      <header>
+        <h1 className="title">Cars</h1>
+      </header>
+      <main className="main">
+        <div className="cars__table">
+          <CarsTable cars={cars} />
+        </div>
+      </main>
     </>
   );
 }
