d72155a45b482ca33df481f739342d6af219248c
Anastasia <nastia.shrai@gmail.com>
2023-06-29 14:56:33 +0300

    feat: create CarsTable component

---
+++ src/components/CarsTable/CarsTable.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import './CarsTable.css';
+import { CarsTableProps } from '../../utils/types';
+
+const CarsTable: FC<CarsTableProps> = ({ cars }) => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th className="table-cell">Company</th>
+          <th className="table-cell">Model</th>
+          <th className="table-cell">VIN</th>
+          <th className="table-cell">Color</th>
+          <th className="table-cell">Year</th>
+          <th className="table-cell">Price</th>
+          <th className="table-cell">Availability</th>
+          <th className="table-cell">Actions </th>
+        </tr>
+      </thead>
+      <tbody>
+        {cars.map((car) => {
+          return (
+            <tr key={car.id}>
+              <td className="table-cell">{car.car}</td>
+              <td className="table-cell">{car.car_model}</td>
+              <td className="table-cell">{car.car_vin}</td>
+              <td className="table-cell">{car.car_color}</td>
+              <td className="table-cell">{car.car_model_year}</td>
+              <td className="table-cell">{car.price}</td>
+              <td className="table-cell">
+                {car.availability === true ? 'Yes' : 'No'}
+              </td>
+              <td className="table-cell">Edit / Delete</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default CarsTable;
