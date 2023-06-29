25a69f06d5e8c739b92ad7c62e9298812af6e5b4
Anastasia <nastia.shrai@gmail.com>
2023-06-29 14:59:58 +0300

    feat: add ts types to handle data

---
+++ src/utils/types.ts
@@ -0,0 +1,15 @@
+export interface CarData {
+  availability: boolean;
+  car: string;
+  car_color: string;
+  car_model: string;
+  car_model_year: number;
+  car_vin: string;
+  id: number;
+  price: string;
+}
+
+export interface CarsTableProps {
+  cars: CarData[];
+  handleModalClick?: () => void;
+}
