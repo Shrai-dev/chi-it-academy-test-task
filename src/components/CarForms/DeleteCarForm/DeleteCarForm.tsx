f2edf9d21f4c23a4490cdadff344a6bf28ef234d
Anastasia <nastia.shrai@gmail.com>
2023-07-04 18:55:07 +0300

    feat: add DeleteCarForm component

---
+++ src/components/CarForms/DeleteCarForm/DeleteCarForm.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import './DeleteCarForm.css';
+import { DeleteCarFormProps } from '../../../utils/types';
+
+const DeleteCarForm: FC<DeleteCarFormProps> = ({ id, handleClick }) => {
+  return (
+    <div className="delete__form-wrapper">
+      <h3 className="delete__form-title">
+        Are you sure you want to delete this car?
+      </h3>
+      <div className="delete__form-btns">
+        <button
+          className="delete__form-btn yes"
+          onClick={() => handleClick(id)}
+        >
+          Yes
+        </button>
+        <button className="delete__form-btn no" onClick={() => handleClick(id)}>
+          No
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteCarForm;
